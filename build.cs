3c1fa08cef340e97489dc834c15e3f59751df562
lesleyanneb <31181282+lesleyanneb@users.noreply.github.com>
2020-11-27 14:57:23 +1100

    Create build.cs

---
+++ build.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace hellocsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello fancy builds!");
+        }
+    }
+}
