a768785eb3fe4a885dcc2ea878368bdd7069d70d
lesleyanneb <31181282+lesleyanneb@users.noreply.github.com>
2020-11-27 15:14:21 +1100

    Update and rename build.cs to program.cs

--- program.cs
+++ program.cs
@@ -6,7 +6,7 @@ namespace building
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello fancy builds!");
+            Console.WriteLine("Hello fancy build!");
         }
     }
 }
